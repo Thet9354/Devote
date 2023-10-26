61eb27a811bb900a6907dc94f2be38e2dc0f771d
PhoonThetPine <thetpine254@gmail.com>
2023-10-26 17:13:21 +0800

    Devote - V2

--- Devote/Extension/HideKeyBoardExtension.swift
+++ Devote/Extension/HideKeyBoardExtension.swift
@@ -5,4 +5,13 @@
 //  Created by Phoon Thet Pine on 19/10/23.
 //
 
-import Foundation
+import SwiftUI
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
+
--- Devote/Utility/Constant.swift
+++ Devote/Utility/Constant.swift
@@ -5,4 +5,24 @@
 //  Created by Phoon Thet Pine on 19/10/23.
 //
 
-import Foundation
+import SwiftUI
+
+// MARK: - FORMATTER
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+var backgroundGradient: LinearGradient {
+    return LinearGradient(gradient: Gradient(colors: [Color.pink, Color.blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+}
+
+// MARK: - UX
+
+let feedback = UINotificationFeedbackGenerator()
+
